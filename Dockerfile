bb3a0582e0b7841ec89cecd3c837f75699a48329
Gsdagustavo <gugadanielalvez@gmail.com>
2026-03-14 22:36:45 -0300

    chore(): Add support for unix builds ; update dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,24 @@
-# Create server executable
 FROM golang:1.26 AS build
 
 WORKDIR /app
 
-COPY . /app
+COPY go.mod go.sum ./
+RUN go mod download
 
-RUN CGO_ENABLED=0 GOOS=linux go build -o server main.go
+COPY . .
 
-# Run server
+RUN CGO_ENABLED=0 GOOS=linux go build -o server .
+
 FROM scratch
 
 WORKDIR /app
 
-COPY --from=build /app/server ./
+ENV CONFIGS_PATH=/app/settings/dev-settings.toml
+
+COPY --from=build /app/server .
+COPY --from=build /app/settings ./settings
+COPY --from=build /etc/ssl/certs/ca-certificates.crt /etc/ssl/certs/
 
 EXPOSE 8000
 
-CMD [ ".server" ]
+CMD ["./server"]
---
+++ unix.go
@@ -0,0 +1,149 @@
+//go:build unix
+
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"log/slog"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/BurntSushi/toml"
+	"github.com/Gsdagustavo/sprinter-api/domain/entities"
+	"github.com/kardianos/service"
+)
+
+func start() error {
+	configsPath := os.Getenv("CONFIGS_PATH")
+	if configsPath == "" {
+		return errors.New("CONFIGS_PATH environment variable not set")
+	}
+
+	cfg := readCFGFile(configsPath)
+
+	file, err := configureOutput(cfg.LogDir)
+	if err != nil {
+		return errors.Join(errors.New("failed to configure log outputs"), err)
+	}
+	defer file.Close()
+
+	s, err := newService(*cfg)
+	if err != nil {
+		return errors.Join(errors.New("failed to create service"), err)
+	}
+
+	err = s.Run()
+	if err != nil {
+		return errors.Join(errors.New("failed to run service"), err)
+	}
+
+	return nil
+}
+
+func (p *program) Start(s service.Service) error {
+	slog.Info("received call to program#start")
+
+	// Start should not block. Do the actual work async.
+	go p.run(false)
+
+	return nil
+}
+
+func (p *program) Stop(s service.Service) error {
+	slog.Info("received call to program#stop")
+
+	// Stop should not block. Return with a few seconds
+	return nil
+}
+
+func configureOutput(logFolder string) (*os.File, error) {
+	if logFolder == "" {
+		return nil, nil
+	}
+
+	now := time.Now()
+	logName := fmt.Sprintf("%s/%s.log", logFolder, now.Format("20060102150405"))
+
+	err := os.MkdirAll(logFolder, os.ModePerm)
+	if err != nil {
+		return nil, errors.Join(errors.New("failed to create log folder"))
+	}
+
+	file, err := os.OpenFile(logName, os.O_RDWR|os.O_CREATE|os.O_APPEND, os.ModePerm)
+	if err != nil {
+		if os.IsNotExist(err) {
+			err = os.WriteFile(logName, []byte(""), os.ModePerm)
+			if err != nil {
+				return nil, errors.Join(errors.New("failed to create log file"))
+			}
+		}
+
+		return nil, errors.Join(errors.New("failed to open log file"), err)
+	}
+
+	log.SetOutput(io.MultiWriter(os.Stdout, file))
+	return file, nil
+}
+
+func readCFGFile(cfgPath string) *entities.Config {
+	file, err := os.Open(cfgPath)
+	if err != nil {
+		panic(err)
+	}
+
+	b, err := io.ReadAll(file)
+	if err != nil {
+		panic(err)
+	}
+
+	err = file.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	var cfg entities.Config
+
+	_, err = toml.Decode(string(b), &cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	return &cfg
+}
+
+func newService(cfg entities.Config) (service.Service, error) {
+	slog.Info("creating service")
+
+	// Load the received arguments
+	var args []string
+
+	// Clean the executable arguments
+	if len(os.Args) > 1 {
+		for _, arg := range os.Args {
+			if strings.Contains(arg, "configs") {
+				args = append(args, arg)
+			}
+		}
+	}
+
+	svcConfig := &service.Config{
+		Name:        "sprinter",
+		DisplayName: "Sprinter API",
+		Arguments:   args,
+	}
+
+	p := &program{
+		cfg: cfg,
+	}
+
+	s, err := service.New(p, svcConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	return s, nil
+}
