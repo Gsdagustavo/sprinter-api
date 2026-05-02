cb0fb0cc47624bfc6f7db4ea6a34e876dec6a1d4
YagoSchramm <yrtoiu1515@gmail.com>
2026-05-02 18:16:42 -0300

    fix(): Fixed the rules for the activity

--- domain/rules/activity.go
+++ domain/rules/activity.go
@@ -9,8 +9,5 @@ func ValidateActivity(activity *entities.Activity) error {
 	if activity.Type != 1 && activity.Type != 2 && activity.Type != 3 {
 		return derr.InvalidActivityType
 	}
-	if activity.StartTime == activity.EndTime {
-		return derr.InvalidActivityDuration
-	}
 	return nil
 }
