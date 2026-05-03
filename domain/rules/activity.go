a99ef8cd83fdd4de82ea67fa13e91425768923c1
YagoSchramm <yrtoiu1515@gmail.com>
2026-05-03 14:32:37 -0300

    fix(): Fix on the activity rules

--- domain/rules/activity.go
+++ domain/rules/activity.go
@@ -30,6 +30,11 @@ func ValidateActivityFinish(activity *entities.Activity) error {
 	if activity.EndDate != time.Now() || activity.EndDate.IsZero() {
 		return derr.InvalidActivityEndDate
 	}
-	
+	for _, point := range activity.Route {
+		if point.Latitude == 0 || point.Longitude == 0 {
+			return derr.InvalidActivityPoint
+		}
+	}
+
 	return nil
 }
